9f7bd8ece58bdd9a13224687e69c8bbb7d22b3b2
sicheng <wsc879453141@gmail.com>
2021-11-29 11:35:50 -0800

    add more

--- GoLibrary.hs
+++ GoLibrary.hs
@@ -1,5 +1,13 @@
 module GoLibrary(
+    Point(Point),
+    Stone(Black, White, Ko, Empty),
+    GoBoard,
+    Move(Pass, Move),
+    Game(Game),
     createGo,
+    getBoard,
+    getStone,
+    getMoveHistory,
     runMove,
     runPass,
     isValidMove,
@@ -22,6 +30,7 @@ data Game = Game {
     boardSize :: Int,
     player :: Stone,
     lastMove :: Move,
+    moveHistory :: [Move],
     scoreBlack :: Int,
     scoreWhite :: Int
 }
@@ -33,29 +42,42 @@ createGo player size = Game{
     boardSize = size,
     player = player,
     lastMove = Pass White,
+    moveHistory = [],
     scoreBlack = 0,
     scoreWhite = 0
 } where points = [(Point x y) | x <- [1..size], y <- [1..size]]
 
+-- get the go board
+getBoard :: Game -> GoBoard
+getBoard game@(Game b bz pl lm mh sb sw) = b
+
+-- get the stone at Point x y
+getStone :: Game -> Point -> Stone
+getStone game@(Game b bz pl lm mh sb sw) point = Map.findKey point b
+
+-- get game move history
+getMoveHistory :: Game -> [Move]
+getMoveHistory game@(Game b bz pl lm mh sb sw) point = mh
 
 -- run a move at (x, y), should check validity first
 runMove :: Game -> Point -> Stone -> Game
 runMove game point stone = removePrisoners (putStone (unlabelAllKo game) point stone) point (getOppositeStone stone)
 
 -- player use pass to show he/she think no more scores can be get
-runPass :: Game -> Stone
-runPass game@(Game b bz pl lm sb sw) stone = Game{
+runPass :: Game -> Game
+runPass game@(Game b bz pl lm mh sb sw) stone = Game{
     board = b,
     boardSize = size,
     player = pl,
     lastMove = Pass stone,
+    moveHistory = mh ++ [Pass stone],
     scoreBlack = if stone == Black then sb else (sb + 1),
     scoreWhite = if stone == While then sw else (sw + 1)
 }
 
 -- check whether a move is valid
 isValidMove :: Game -> Point -> Stone -> Bool
-isValidMove game@(Game b bz pl lm sb sw) point stone 
+isValidMove game@(Game b bz pl lm mh sb sw) point stone 
     | not (stone == turnPlayer && stone == pl) = False
     | Map.findKey point b != Empty             = False
     | length (findDead game point stone) == 0  = True
@@ -71,22 +93,30 @@ isValidMove game@(Game b bz pl lm sb sw) point stone
 
 -- after both player used pass, the game end, use finishGo to count scores
 finishGo :: Game -> Game
-
+finishGo game@(Game b bz pl lm mh sb sw) = Game {
+    board = b,
+    boardSize = size,
+    player = pl,
+    lastMove = lm,
+    moveHistory = mh,
+    scoreBlack = sb + blackTerritory,
+    scoreWhite = sw + whiteTerritory
+} where (blackTerritory, whiteTerritory) = countTerritory game
 
 -- after finishGo, use getWinner
 getWinner :: Game -> Stone
-
-
-
+getWinner game@(Game b bz pl lm mh sb sw)
+    | sb > sw + 7 = Black
+    | otherwise   = White
 
 
 turnPlayer :: Game -> Stone
-turnPlayer game@(Game b bz pl lm sb sw) = case lm of Pass stone -> getOppositeStone stone
+turnPlayer game@(Game b bz pl lm mh sb sw) = case lm of Pass stone -> getOppositeStone stone
                                                     Move point stone -> getOppositeStone stone
 
 
 removePrisoners :: Game -> Point -> Stone -> Game
-removePrisoners game@(Game b bz pl lm sb sw) point prison_stone
+removePrisoners game point prison_stone
     | length removeAll == 1 && isKo = updateScore (labelKo (removeStones game removeAll) (removeAll !! 0)) 1 attack_stone
     | otherwise = updateScore (removeStones game removeAll) (length removeAll) attack_stone
     where 
@@ -121,21 +151,23 @@ rightPoint :: Point -> Point
 rightPoint (Point x y) = Point x y+1
 
 putStone :: Game -> Point -> Stone -> Game
-putStone game@(Game b bz pl lm sb sw) point stone = Game{
+putStone game@(Game b bz pl lm mh sb sw) point stone = Game{
     board = putStone' b point stone,
     boardSize = bz,
     player = pl,
     lastMove = Move point stone,
+    moveHistory = mh ++ [Move point stone],
     scoreBlack = sb,
     scoreWhite = sw
 }
 
 removeStone :: Game -> Point -> Game
-removeStone game@(Game b bz pl lm sb sw) point stone = Game{
+removeStone game@(Game b bz pl lm mh sb sw) point stone = Game{
     board = putStone' b point Empty,
     boardSize = bz,
     player = pl,
     lastMove = lm,
+    moveHistory = mh,
     scoreBlack = sb,
     scoreWhite = sw
 }
@@ -145,21 +177,23 @@ removeStone game []      = game
 removeStones game (p:ps) = removeStones (removeStone game p) ps
 
 labelKo :: Game -> Point -> Game
-labelKo game@(Game b bz pl lm sb sw) point stone = Game{
+labelKo game@(Game b bz pl lm mh sb sw) point stone = Game{
     board = putStone' b point Ko,
     boardSize = bz,
     player = pl,
     lastMove = lm,
+    moveHistory = mh,
     scoreBlack = sb,
     scoreWhite = sw
 }
 
 unlabelAllKo :: Game -> Game
-unlabelAllKo game@(Game b bz pl lm sb sw) point stone = Game{
+unlabelAllKo game@(Game b bz pl lm mh sb sw) point stone = Game{
     board = if ko_points == [] then b else putStone' b (fmap fst ko_points) Empty,
     boardSize = bz,
     player = pl,
     lastMove = lm,
+    moveHistory = mh,
     scoreBlack = sb,
     scoreWhite = sw
 } where ko_points = List.filter (\(p, s) -> s == Ko) (assocs b)
@@ -179,9 +213,9 @@ getOppositeStone stone
     | otherwise = Empty
 
 updateScore :: Game -> Int -> Stone -> Game
-updateScore game@(Game b bz pl lm sb sw) score stone
-    | stone == Black = (Game b bz pl lm (sb + score) sw)
-    | otherwise      = (Game b bz pl lm sb (sw + score))
+updateScore game@(Game b bz pl lm mh sb sw) score stone
+    | stone == Black = (Game b bz pl lm mh (sb + score) sw)
+    | otherwise      = (Game b bz pl lm mh sb (sw + score))
 
 findDead :: Game -> Point -> Stone -> [Point]
 findDead game point prison_stone = 
@@ -197,14 +231,56 @@ purifyPoints [] = []
 purifyPoints ((Just p):ps) = p:(purifyPoints ps)
 
 bfsSearchDead :: Game -> Point -> Stone -> [Maybe Point] -> [Maybe Point]
-bfsSearchDead game@(Game b bz pl lm sb sw) point@(Point x y) prison_stone seen_points
+bfsSearchDead game@(Game b bz pl lm mh sb sw) point@(Point x y) prison_stone seen_points
     | x < 1 || x > bz || y < 1 || y > bz             = seen_points
     | elem (Just point) seen_points                  = seen_points
-    | purify $ Map.findKey point b == Empty          = Nothing:seen_points
-    | purify $ Map.findKey point b == Ko             = Nothing:seen_points
-    | purify $ Map.findKey point b /= prison_stone   = seen_points
+    | Map.findKey point b == Empty                   = Nothing:seen_points
+    | Map.findKey point b == Ko                      = Nothing:seen_points
+    | Map.findKey point b /= prison_stone            = seen_points
     | otherwise = bfsSearchDead game (upPoint point)
         $ bfsSearchDead game (downPoint point)
         $ bfsSearchDead game (leftPoint point)
         $ bfsSearchDead game (rightPoint point)
         ((Just point):seen_points)
+
+countTerritory :: Game -> (Int, Int)
+countTerritory game = (blackTerritory, whiteTerritory)
+    where
+        blackTerritory = length $ filter (\x == Black) territory
+        whiteTerritory = length $ filter (\x == White) territory
+        territory = fmap (decideTerritory game) points
+        points = [(Point x y) | x <- [1..size], y <- [1..size]]
+
+decideTerritory :: Game -> Point -> Stone
+decideTerritory game@(Game b bz pl lm mh sb sw) point
+    | Map.findKey point b == Black  = Black
+    | Map.findKey point b == White  = White
+    | blackNeighbor > whiteNeighber = Black
+    | blackNeighbor < whiteNeighber = White
+    | otherwise                     = Empty
+    where 
+        blackNeighbor = countNeighbor game point Black
+        whiteNeighber = countNeighbor game point White
+
+countNeighbor :: Game -> Point -> Stone
+countNeighbor game point stone = countAll 
+    where  
+        countAll = countUp + countDown + countLeft + countRight
+        countUp = countOneDirection game point (-1, 0) stone
+        countDown = countOneDirection game point (1, 0) stone
+        countLeft = countOneDirection game point (0, -1) stone
+        countRight = countOneDirection game point (0, 1) stone
+
+countOneDirection :: Game -> Point -> (Int, Int) -> Stone
+countOneDirection game@(Game b bz pl lm mh sb sw) point@(Point x y) (xx, yy) stone
+    | Map.findKey newpoint b == Nothing                = 0
+    | Map.findKey newpoint b == stone                  = 1
+    | Map.findKey newpoint b == getOppositeStone stone = 0
+    | otherwise                                        = countOneDirection game newpoint (xx, yy) stone
+    where
+        newpoint = Point (x + xx) (y + yy)
+
+
+
+        
+    
