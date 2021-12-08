7ac214a066cdac3601c145657350eabc9af87160
qsdrqs <qsdrqs@gmail.com>
2021-12-08 10:59:53 -0800

    merge other branchs

--- app/Main.hs
+++ app/Main.hs
@@ -1,13 +1,41 @@
 module Main where
 
-import Lib
-import Networks
-import System.Environment
+import Brick 
+    (App(..), attrMap
+    )
+import qualified Brick.Main as M
+import UI 
+    (GameState, ResourceName, getInitialState
+    ,Tick
+    ,drawUi
+    ,handleEvent
+    ,cursor
+    )
+import qualified Graphics.Vty as V
+import Control.Monad (void)
 
+-- ui :: Widget ()
+-- ui = str "Hello, world!"
+
+app :: App GameState Tick ResourceName
+app = App {
+    appDraw = drawUi,
+    -- appChooseCursor = M.neverShowCursor,
+    appChooseCursor = cursor,
+    appHandleEvent = handleEvent,
+    appStartEvent = return,
+    appAttrMap = const $ attrMap V.defAttr []
+}
+
+
+main :: IO ()
 main = do
-  args <- getArgs
-  if head args == "client"
-     then do
-         clientPlayer
-  else do
-         serverPlayer
+    -- enable mouse event
+    let buildVty = do {
+        v <- V.mkVty =<< V.standardIOConfig;
+        V.setMode (V.outputIface v) V.Mouse True;
+        return v;
+    }
+        
+    initialVty <- buildVty
+    void $ M.customMain initialVty buildVty Nothing app $ getInitialState
